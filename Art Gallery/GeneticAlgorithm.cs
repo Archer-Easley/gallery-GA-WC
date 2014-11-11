6cf208961e754d06cbce7de5c255b05a43525a7f
Stephen Archer <starcher91@gmail.com>
2014-11-11 18:45:09 -0500

    Crossover stub filled out

--- Art Gallery/GeneticAlgorithm.cs
+++ Art Gallery/GeneticAlgorithm.cs
@@ -36,7 +36,41 @@ namespace FormsPolygonGenerator
 
         private void crossover(List<Vertex> points1, List<Vertex> points2)
         {
+            //create temporary copies
+            List<Vertex> temp1 = new List<Vertex>(points1);
+            List<Vertex> temp2 = new List<Vertex>(points2);
+            List<Vertex> temp1Orig = new List<Vertex>(points1);
 
+            int randPoint = r.Next(temp1.Count);
+
+            //swap items in first copy
+            for (int i = 0; i < randPoint; i++)
+            {
+                if (temp2[i].hasGuard)
+                {
+                    temp1[i].hasGuard = true;
+                }
+                else
+                {
+                    temp1[i].hasGuard = false;
+                }
+            }
+
+            //swap items in second array
+            for (int i = 0; i < randPoint; i++)
+            {
+                if (temp1Orig[i].hasGuard)
+                {
+                    temp2[i].hasGuard = true;
+                }
+                else
+                {
+                    temp2[i].hasGuard = false;
+                }
+            }
+
+            population.Add(temp1);
+            population.Add(temp2);
         }
 
         public void performGA()
